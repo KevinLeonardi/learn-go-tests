621b10c93d5e44e3acae2bc16a61b6767067bfcd
Kevin Leonardi <kevinleonardi@yahoo.com>
2024-04-26 21:36:03 +0700

    rectangle.Area undefined

---
+++ structs-methods-interfaces/shapes.go
@@ -0,0 +1,21 @@
+package structsmethodsinterfaces
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Area(circle Circle) float64       {}
+func Area(rectangle Rectangle) float64 {}
