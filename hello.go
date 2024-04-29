daafb919358a88eeab4388e73f66aa0ff5d01f19
Kevin Leonardi <kevin.leonardi@pt-ssss.com>
2024-04-29 11:47:29 +0700

    hello, iteration, array-slices chaptere

---
+++ hello.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	}
+	return prefix + name
+}
+func main() {
+	fmt.Println("hello world")
+}
